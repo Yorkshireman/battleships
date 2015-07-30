aa6e94a37b46765f16ed6c774813229595930a54
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-07-30 13:13:29 +0100

    ships are constrained to the board

--- lib/board.rb
+++ lib/board.rb
@@ -2,15 +2,19 @@ require_relative './ship.rb'
 
 class Board
 
-  attr_reader :grid
+  attr_reader :ships
 
   def initialize
-    @grid = {}
+    @ships = {}
   end
 
   def place_ship ship, location, direction
     squares = occupies_squares(ship, location, direction)
-    grid.store(ship, squares)
+    if outside_board?(squares)
+      fail "Cannot place there - outside the board"
+    else  
+      ships.store(ship, squares)
+    end
   end
 
   def occupies_squares ship, location, direction
@@ -21,18 +25,6 @@ class Board
       x = 0
         ( ship_size(ship.type) - 1 ).times do
         squares << (squares[x]).next
-        x += 1
-      end
-    end
-
-    if direction == :N
-      x = 0
-        ( ship_size(ship.type) - 1 ).times do
-        location = location.to_s
-        first_char = location[0]
-        second_char = (location[1].to_i - 1).to_s
-        location = (first_char + second_char).to_sym
-        squares << location
         x += 1
       end
     end
@@ -48,24 +40,23 @@ class Board
         x += 1
       end
     end
-
-    if direction == :W
-      x = 0
-        ( ship_size(ship.type) - 1 ).times do
-        location = location.to_s
-        first_char = (location[0].ord - 1).chr
-        second_char = location[1]
-        location = (first_char + second_char).to_sym
-        squares << location
-        x += 1
-      end
-    end
     
     squares
   end
 
+  def outside_board?(squares)
+    squares.each do |square|
+      valid_squares = [:A1, :A2, :A3, :A4, :A5, :A6, :A7, :A8, :A9, :B1, :B2, :B3, :B4, :B5, :B6, :B7, :B8, :B9, :C1, :C2, :C3, :C4, :C5, :C6, :C7, :C8, :C9, :D1, :D2, :D3, :D4, :D5, :D6, :D6, :D7, :D8, :D9, :E1, :E2, :E3, :E4, :E5, :E6, :E7, :E8, :E9, :F1, :F2, :F3, :F4, :F5, :F6, :F7, :F8, :F9, :G1, :G2, :G3, :G4, :G5, :G6, :G7, :G8, :G9, :H1, :H2, :H3, :H4, :H5, :H6, :H7, :H8, :H9, :I1, :I2, :I3, :I4, :I5, :I6, :I7, :I8, :I9]
+      unless valid_squares.include?(square)
+        return true
+      else
+        return false
+      end
+    end
+  end
+
   def locate_ship ship
-    grid[ship]
+    ships[ship]
   end
 
   def ship_size type
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,10 +7,25 @@ describe Board do
 
   it 'can have a ship' do
     subject.place_ship(patrol_boat, :B1, :S)
-    expect(subject.grid).to include patrol_boat
+    expect(subject.ships).to include patrol_boat
   end
 
-  describe '#locate_ship?' do
+  it 'ships cannot be placed outside the board' do 
+    expect{subject.place_ship(patrol_boat, :L13, :S)}.to raise_error "Cannot place there - outside the board"
+  end
+
+  describe '#outside_board?' do 
+    it 'returns false when passed valid squares' do 
+      valid_squares = [:A1, :A2, :A3, :A4, :A5, :A6, :A7, :A8, :A9, :B1, :B2, :B3, :B4, :B5, :B6, :B7, :B8, :B9, :C1, :C2, :C3, :C4, :C5, :C6, :C7, :C8, :C9, :D1, :D2, :D3, :D4, :D5, :D6, :D6, :D7, :D8, :D9, :E1, :E2, :E3, :E4, :E5, :E6, :E7, :E8, :E9, :F1, :F2, :F3, :F4, :F5, :F6, :F7, :F8, :F9, :G1, :G2, :G3, :G4, :G5, :G6, :G7, :G8, :G9, :H1, :H2, :H3, :H4, :H5, :H6, :H7, :H8, :H9, :I1, :I2, :I3, :I4, :I5, :I6, :I7, :I8, :I9]
+      expect(subject.outside_board? valid_squares).to be false
+    end
+
+    it 'returns true when passed a mixture of valid and invalid squares' do 
+      expect(subject.outside_board? [:J3, :A11, :A3]).to be true
+    end
+  end
+
+  describe '#locate_ship' do
 
     context 'after placing a destroyer facing south' do
       it 'returns the correct three squares' do
@@ -19,24 +34,10 @@ describe Board do
       end
     end
 
-    context 'after placing a destroyer facing north' do
-      it 'returns the correct three squares' do
-        subject.place_ship(destroyer, :D5, :N)
-        expect(subject.locate_ship(destroyer)).to eq [:D5, :D4, :D3]
-      end
-    end
-
     context 'after placing a destroyer facing east' do
       it 'returns the correct three squares' do
         subject.place_ship(destroyer, :A1, :E)
         expect(subject.locate_ship(destroyer)).to eq [:A1, :B1, :C1]
-      end
-    end
-
-    context 'after placing a destroyer facing west' do
-      it 'returns the correct three squares' do
-        subject.place_ship(destroyer, :C3, :W)
-        expect(subject.locate_ship(destroyer)).to eq [:C3, :B3, :A3]
       end
     end
 
